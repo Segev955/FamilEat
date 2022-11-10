d918895e4c055c6315218027aebbd206aeb5b4cc
SaliSharfman <salishar2@gmail.com>
2022-11-10 16:03:44 +0200

    login and register activities

--- app/src/main/java/com/example/famileat/MainActivity.java
+++ app/src/main/java/com/example/famileat/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.famileat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
+    private Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent n = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(n);
+            }
+        });
+        register = findViewById(R.id.register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent n = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(n);
+            }
+        });
     }
+
 }
