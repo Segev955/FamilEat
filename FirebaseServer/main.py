f6c158fff109f96afac23001c7d2370c3a10b3cc
Segev955 <Segev95@gmail.com>
2023-01-18 18:04:20 +0200

    server login

--- FirebaseServer/main.py
+++ FirebaseServer/main.py
@@ -63,7 +63,8 @@ async def login(request: Request):
    password = req_json['password']
    try:
        user = pb.auth().sign_in_with_email_and_password(email, password)
-       jwt = user['idToken']
+       jwt = user['localId']
+       print(user)
        return JSONResponse(content={'token': jwt}, status_code=200)
    except:
        return HTTPException(detail={'message': 'There was an error logging in'}, status_code=400)
